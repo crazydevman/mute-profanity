71f65d4a5a6ce509f3b710032c82aa00e71fd40f
Brian <github.co.syspod@recursor.net>
2012-08-21 15:53:15 +0700

    Added ability to extract srt from mp4 and m4v using mp4box
    Copied and modified code for mkvsubextract to use mp4box. Tested
    outside of XBMC and it works, but won't work in XBMC because the Mac
    version uses the python 2.6 version and subprocess.check_output doesn't
    work.

---
+++ resources/lib/MP4SubExtract.py
@@ -0,0 +1,59 @@
+import os
+import re
+import subprocess
+
+
+def getSubTrack(filePath, toolsDir):
+    if not os.path.isdir(toolsDir):
+        toolsDir = os.path.split(toolsDir)
+    
+    infoPath = os.path.join(toolsDir, "mp4box")
+    output = subprocess.check_output([infoPath, "-info", filePath])
+
+    tracks = {}
+    trackNumber = None
+    for line in output.splitlines():
+        r = re.search('Track # (\d+)', line)
+        if r:
+            trackNumber = int(r.group(1))
+            trackID = trackNumber
+            r = re.search('TrackID (\d+)', line)
+            if r:
+                trackID = int(r.group(1))
+            tracks[trackNumber] = { 'TID': trackID }
+            continue
+
+        r = re.search('Language "(.*)" - Type "', line)
+        if r:
+            language = r.group(1)
+            tracks[trackNumber]['language'] = language
+            continue
+
+        r = re.search('- Type "(.+)"', line)
+        if r:
+            trackType = r.group(1)
+            tracks[trackNumber]['type'] = trackType
+            continue
+
+    subTrackID = None
+    for track in tracks.values():
+        if track['type'] != 'text:tx3g':
+            continue
+        if 'language' in track and track['language'] != 'English':
+            continue
+        subTrackID = track['TID']
+        break
+            
+    if subTrackID != None:
+        print 'Found subtitle track: %d' % subTrackID
+    return subTrackID
+
+def extractFromMP4(filePath, toolsDir, trackID):
+    
+    if not os.path.isdir(toolsDir):
+        toolsDir = os.path.split(toolsDir)    
+    
+    extractPath = os.path.join(toolsDir, "mp4box")
+    srtPath = os.path.splitext(filePath)[0] + ".srt"
+    return subprocess.call([extractPath, "-srt", str(trackID), filePath , srtPath])
+
